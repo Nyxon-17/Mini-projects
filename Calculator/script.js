e4c0d52f251a032122eee76250119ab7424ba2d9
Nyxon-17 <sahidff0.n@gmail.com>
2026-08-08 16:01:09 +0530

    calculator

---
+++ Calculator/script.js
@@ -0,0 +1,28 @@
+let string = "";
+
+let buttons = document.querySelectorAll(".button");
+let input = document.querySelector(".display")
+
+buttons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+        if (e.target.innerHTML == "=") {
+            string = eval(string)
+            input.value = string;
+            string ="";
+
+        } else if (e.target.innerHTML == "AC"){
+            string ="";
+            input.value =string;
+        }
+        else if(e.target.innerHTML == ""){
+            string = string.slice(0, -1);
+            input.value = string 
+        }
+        else {
+            string = string + e.target.innerHTML
+            input.value = string;
+        }
+        
+    })
+
+})
